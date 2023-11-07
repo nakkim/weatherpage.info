2e159154a3b6b8b0035019f46e7f28f058e0af27
Ville Oravilkka <ville.oravilkka@fmi.fi>
2023-11-07 09:48:30 +0200

    Remove commented code

--- src/components/CloudCover.tsx
+++ src/components/CloudCover.tsx
@@ -10,11 +10,10 @@ import { ReactComponent as CloudCover8 } from "../assets/nn/8.svg";
 import { ReactComponent as CloudCover9 } from "../assets/nn/9.svg";
 
 interface IProps {
-  value: number | string | undefined
+  value: number | string | undefined;
 }
 
 const CloudCover: React.FC<IProps> = ({ value }) => {
-
   if (value === 0) return <CloudCover0 />;
   if (value === 1) return <CloudCover1 />;
   if (value === 2) return <CloudCover2 />;
@@ -26,24 +25,6 @@ const CloudCover: React.FC<IProps> = ({ value }) => {
   if (value === 8) return <CloudCover8 />;
   if (value === 9) return <CloudCover9 />;
   return <CloudCover9 />;
-
-  // return (
-    // <svg>
-    //   <circle cx="32" cy="32" r="28" transform="matrix(0.914,0,0,0.914,-1.76,-1.8)" id="circle3790"/>
-    // </svg>
-    // <svg width="50px" height="50px" viewBox="0 0 42 42" className="donut" aria-labelledby="beers-title beers-desc" role="img">
-    //     <circle className="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="white" role="presentation"></circle>
-    //     <circle className="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" strokeWidth="3" role="presentation"></circle>
-    //     <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#ce4b99" strokeWidth="3"  strokeDashoffset="25" aria-labelledby="donut-segment-1-title donut-segment-1-desc">
-    //     </circle>
-    //     <g className="chart-text">
-    //       <text className="chart-number" x="35%" y="60%">
-    //         jeejee
-    //       </text>
-    //     </g>
-    //   </svg>
-  // )
-
 };
 
-export default CloudCover
+export default CloudCover;
